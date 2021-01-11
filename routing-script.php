211d825a804fbfbac30115f5e4a13098d8627f37
Johny Thomas Kariath <34306437+tomkariath@users.noreply.github.com>
2021-01-11 23:10:36 +0530

    PHP script updated

--- routing-script.php
+++ routing-script.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $username = "username";
-    $password = "password";
-    $dbname = "myDB";
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "oracle123@";
+    $dbname = "fieldService";
 
     $connection = new mysqli($servername, $username, $password, $dbname);
 
@@ -10,8 +10,8 @@
         die("Connection Failed: ". $connection->connect_error);
     }
 
-    $query = "MERGE INTO ACTIVITY_TYPE (ACTIVITY_TYPE_CODE, ACTIVITY_TYPE_NAME, DURATION, COLOR, IS_ENABLED, AVAILABLE_TIME_SLOT) 
-    VALUES (101, 'Installation', 3, '#1abc9c', true, '1-16')";
+    $query = "INSERT INTO ACTIVITY_TYPE (ACTIVITY_TYPE_CODE, ACTIVITY_TYPE_NAME, DURATION, COLOR, IS_ENABLED, AVAILABLE_TIME_SLOT) 
+    VALUES (102, 'Upgrade', 3, '#3498db', true, '11-16')";
     
     if($connection->query($query)==TRUE){
         echo "Success";
